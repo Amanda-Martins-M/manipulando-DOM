c959787571b6facba8eabc00216312d726890fd4
Amanda Martins <amandac.martins.moura@gmail.comgit config --global user.email amandac.martins.moura@gmail.com>
2025-12-13 15:25:10 -0300

    Ajuste no arquivo CSS e JS

---
+++ js/menu-hamburguer.js
@@ -0,0 +1,34 @@
+/*     
+        <li>Ler ou alterar conteúdo</li>
+        <li>Alterar atributos</li>
+        <li>Criar ou remover elementos</li>
+        */
+
+/* Selecionar elementos */
+const btnMenu = document.getElementById("btn-menu");
+const listaMenu = document.querySelector("#menu-principal");
+const iconeBtnMenu = document.querySelector("i");
+
+/* Adicionar eventos (ou seja, observar ações do usuário)*/
+btnMenu.addEventListener("click", function() {
+    //Alterar estilos
+   // listaMenu.style.display = "block";
+   // listaMeny.style.backgroundColor = "#ccc";
+listaMenu.classList.toggle("ativo");
+   // Manipulando classes
+   if(listaMenu.classList.contains("ativo")){
+   iconeBtnMenu.classList.remove("fi-sr-menu-burger");
+   iconeBtnMenu.classList.add("fi-sr-cross");
+
+   // Alterando o valor dos atributos
+   btnMenu.setAttribute("aria-label", "Fechar Menu");
+   btnMenu.setAttribute("aria-expanded", "true");
+} else{
+    iconeBtnMenu.classList.remove("fi-sr-cross");
+    iconeBtnMenu.classList.add("fi-sr-menu-burger");
+
+    btnMenu.setAttribute("aria-label", "Abrir Menu");
+    btnMenu.setAttribute("aria-expanded", "false");
+}
+
+});
--- menu-hamburguer.js
+++
@@ -1,7 +0,0 @@
-const btnMenu = document.getElementById('btn-menu');
-const listaMenu = document.querySelector("#menu-principal");
-
-btnMenu.addEventListener('click', () => {
-    
-});
-
